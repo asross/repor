8f6d2890fcbf3e824ad655db1aad15267fcf5603
chaunce <chaunce.slc@gmail.com>
2020-01-02 11:52:45 -0700

    nested hash serializer should return a hash

--- lib/repor/serializer/nested_hash.rb
+++ lib/repor/serializer/nested_hash.rb
@@ -2,9 +2,9 @@ module Repor
   module Serializer
     class NestedHash < Base
       def table
-        report.hashed_data.collect { |row| row.map { |k,v| [k, (v.try(:min) || v).to_s] }.to_h}.collect do |row|
+        report.hashed_data.collect { |row| row.map { |k,v| [k, (v.try(:min) || v).to_s] }.to_h }.collect do |row|
           report.grouper_names.reverse.inject(row.slice(*report.aggregators.keys)) { |nested_row_data, group| { row[group] => nested_row_data } }
-        end
+        end.reduce({}, :merge)
       end
     end
   end
