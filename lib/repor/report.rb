d77eff61a62f5d0270ce1dc1626f6abd0e412d92
chaunce <chaunce.slc@gmail.com>
2020-01-06 12:45:14 -0700

    update formating

--- lib/repor/report.rb
+++ lib/repor/report.rb
@@ -24,12 +24,12 @@ module Repor
       @parent_groupers = @params.delete(:parent_groupers) || ( grouper_names & Array(parent_report&.grouper_names) )
 
       # You may pass in pre-compiled :row_data if you don't want Repor to compile this data for you. All :calculators
-      # and :trackers will still be processed when :raw_data is passed in
+      # and :trackers will still be processed when :raw_data is passed in.
       @raw_data = @params.delete(:raw_data)
 
       # You may pass in pre-aggregated :total_report object as an instance of Repor::Report if you don't want Repor to
       # total this data for you no additional processing is completed on #total_report when a :total_report value is
-      # passed
+      # passed.
       @total_report = @params.delete(:total_report)
 
       # Instead or in addition to passing a :total_report you may pass :total_data, which is used when report data is
@@ -43,10 +43,13 @@ module Repor
       # After params are parsed and validated you can call #data (or any derivitive of: #raw_data, #flat_data,
       # #hashed_data, #nested_data, etc.) on the Repor::Report object to #aggregate the data. This will aggregate all
       # the raw data by the configured dimensions, process any calculators, and then process any trackers.
-      # Caclulators calculate values using the current row data and the #parent_report
-      # Trackers calculate values using the current row data and prior row data
+      
+      # Caclulators calculate values using the current row data and the #parent_report.
+      
+      # Trackers calculate values using the current row data and prior row data.
 
-      # If pre-compiled raw data was passed in, process all :calculators and :trackers now
+
+      # If pre-compiled raw data was passed in, process all :calculators and :trackers now.
       aggregate if  @raw_data.present? && ( @params.include?(:calculators) || @params.include?(:trackers) )
       total if @total_data.present?
     end
