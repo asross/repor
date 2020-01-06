7653b739990733d41f836fc6592066c8b613f282
chaunce <chaunce.slc@gmail.com>
2020-01-06 12:34:16 -0700

    use simplecov gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,26 @@
 # Configure Rails Environment
 ENV["RAILS_ENV"] ||= "test"
 
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+
+  add_group 'Report', ['lib/repor.rb', 'lib/repor/report', 'lib/repor/invalid_params_error.rb', 'lib/repor/version.rb']
+
+  add_group 'Aggregators', 'lib/repor/aggregator'
+  add_group 'Calculators', 'lib/repor/calculator'
+  add_group 'Trackers', 'lib/repor/tracker'
+
+  add_group 'Dimensions', 'lib/repor/dimension'
+
+  add_group 'Serializers', 'lib/repor/serializer'
+
+  add_group 'Tasks', 'lib/repor/tasks'
+
+  add_group "Long files" do |src_file| src_file.lines.count > 100 end
+  add_group "Short files" do |src_file| src_file.lines.count < 5 end
+end
+
 require File.expand_path("../../spec/dummy/config/environment.rb",  __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../spec/dummy/db/migrate", __FILE__)]
 require "rails/test_help"
