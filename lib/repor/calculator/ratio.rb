9fae8e15d6da85e6d6c8ad4c20d19159a8614746
chaunce <chaunce.slc@gmail.com>
2020-01-21 20:07:58 -0700

    ratio is calcualtated by comparing a parent row and does not use a sql expression

--- lib/repor/calculator/ratio.rb
+++ lib/repor/calculator/ratio.rb
@@ -1,12 +1,8 @@
 module Repor
   module Calculator
     class Ratio < Repor::Calculator::Base
-      def function
-        "SUM(#{expression})"
-      end
-
       def calculate(row, parent_row)
-        ((row[aggregator].to_f / parent_row[parent_aggregator].to_f) * 100) unless parent_row[parent_aggregator].to_f == 0
+        ((row[aggregator].to_f / parent_row[parent_aggregator].to_f) * 100) unless parent_row[parent_aggregator].to_f.zero?
       end
     end
   end
