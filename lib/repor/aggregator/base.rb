45aaba69e7484059693515d86162d8c2c30e56a8
chaunce <chaunce.slc@gmail.com>
2020-01-27 18:32:50 -0700

    aggregators and data dimensions now support a model+attribute and/or table_name+column configuration combonations, these will replace the expression option in v1.0

--- lib/repor/aggregator/base.rb
+++ lib/repor/aggregator/base.rb
@@ -7,6 +7,7 @@ module Repor
         @name = name
         @report = report
         @opts = opts
+        validate_params!
       end
 
       def sql_value_name
@@ -23,6 +24,12 @@ module Repor
 
       private
 
+      def validate_params!
+        if opts.include?(:expression)
+          ActiveSupport::Deprecation.warn("passing an :expression option will be deprecated in version 1.0\n  please use :attribute, and, when required, :model or :table_name")
+        end
+      end
+
       def relate(groups)
         relation.call(groups)
       end
@@ -31,8 +38,35 @@ module Repor
         opts.fetch(:relation, ->(r) { r })
       end
 
+      def model
+        opts.fetch(:model, report.report_model)
+      end
+
+      def attribute
+        opts.fetch(:attribute, name)
+      end
+
+      def table_name
+        return @table_name unless @table_name.nil?
+
+        @table_name = opts[:table_name]
+        @table_name = model.try(:table_name) if @table_name.nil?
+        @table_name = model.to_s.constantize.try(:table_name) rescue nil if @table_name.nil?
+        @table_name = report.table_name if @table_name.nil?
+
+        @table_name
+      end
+
+      def column
+        opts.fetch(:column, attribute)
+      end
+
       def expression
-        opts.fetch(:expression, "#{report.table_name}.#{name}")
+        opts.fetch(:expression, "#{table_name}.#{column}")
+      end
+
+      def enum?
+        false # Hash(model&.defined_enums).include?(attribute.to_s)
       end
     end
   end
