43434e6c50993fbdafd6ce94e5a356523901dd35
chaunce <chaunce.slc@gmail.com>
2020-09-15 18:46:14 -0600

    update serializers to use empty string instead of class name for time dimension with nil min and max (grouping a nil date value)

--- lib/repor/serializer/nested_hash.rb
+++ lib/repor/serializer/nested_hash.rb
@@ -2,7 +2,7 @@ module Repor
   module Serializer
     class NestedHash < Base
       def table
-        report.hashed_data.collect { |row| row.map { |k,v| [k, (v.try(:min) || v).to_s] }.to_h }.collect do |row|
+        report.hashed_data.collect { |row| row.map { |k,v| [k, (v.respond_to?(:min) ? v.min : v).to_s] }.to_h }.collect do |row|
           report.grouper_names.reverse.inject(row.slice(*report.aggregators.keys)) { |nested_row_data, group| { row[group] => nested_row_data } }
         end.reduce({}, :merge)
       end
